c6c46d22437aeea9c5ded3bd6bfd8ae3edcc574c
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-12-28 20:06:35 +0100

    changes size of letters in string, first letter is big second small third big and so on.

---
+++ Open-Lab-05.05/Open-Lab-05.05/StringTools.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Open_Lab_05._05
+{
+    public class StringTools
+    {
+        public string AlternatingCaps(string original)
+        {
+            int repeat = 0;
+            string final = "";
+            string a = "";
+            original = original.ToLower();
+            foreach (char c in original)
+            {
+                if (c.ToString() != " ")
+                {
+                    repeat++;
+                }
+                
+                if (repeat % 2 == 0)
+                {
+                    a = c.ToString();
+                }
+                else
+                {
+                    a = c.ToString().ToUpper();
+                }
+
+                final += a;
+            }
+
+            return final;
+        }
+    }
+}
